f17529c6da85e47c2647aa3df191251303154cc1
Jowita Lapies <jowita.lapies@gmail.com>
2019-05-06 12:44:53 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -5,8 +5,8 @@ def my_select(collection)
   select = []
   while i < collection.length
     if yield(collection[i]) == true
-    select << (collection[i])
-  end
+      select << (collection[i])
+    end
     i += 1
   end
   select
